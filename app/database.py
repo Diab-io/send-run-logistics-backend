4e900ae827951ea3072488cc7928e875c9525d33
David Odimayo <odimayodavid7@gmail.com>
2026-05-23 15:19:21 +0100

    refactor: Made database engine connection use pooling

--- app/database.py
+++ app/database.py
@@ -7,9 +7,9 @@ settings = get_settings()
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=True,
-    connect_args={
-        "statement_cache_size": 0
-    }
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=10,
 )
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
